ce4c897dbd14559a7991e41a24323a8e5f7d8b28
Timilehin OJO <timilehin175@gmail.com>
2023-10-26 04:53:30 +0100

    updated users.js

--- routes/users.js
+++ routes/users.js
@@ -23,6 +23,13 @@ const userDetails= [
         gender: 'm',
         id: 3,
         role: 'user'
+    },
+    {
+        name: "tolu Ojo",
+        email: "tolu@gmail.com",
+        gender: "f",
+        id: 4,
+        role: "user"
     }
 ]
 
@@ -32,6 +39,29 @@ userRouter.get('/', (req, res) => {
     res.json(userDetails)
 })
 
+//get one user by id
+userRouter.get('/:id', (req, res) => {
+    const id = req.params.id;
+    const user = userDetails.find(user => user.id == id)
 
+    if (!user){
+        res.status(404)
+        res.send('User with this id not found')
+        return
+    }
 
+    res.json(user)
+})
+
+//Post a user
+userRouter.post('/', (req, res) => {
+    const user = req.body
+    userDetails.push(user)
+    res.json(userDetails)
+})
+
+//update a user details
+userDetails.put('/:id',(req, res) => {
+    const id = req.params.id
+})
 module.exports = userRouter
